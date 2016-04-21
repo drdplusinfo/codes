6bed228b265a90cd41ade1979adb93e8cc981b8c
Jaroslav Tyc <mail@jaroslavtyc.com>
2016-04-21 12:40:25 +0200

    Activity type codes

---
+++ DrdPlus/Tests/Codes/ActivityTypeCodesTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace DrdPlus\Tests\Codes;
+
+use DrdPlus\Codes\ActivityTypeCodes;
+
+class ActivityTypeCodesTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function I_can_get_all_activity_codes()
+    {
+        self::assertSame(
+            $expectedActivityCodes = [
+                'sleeping_or_rest_in_bed',
+                'lounging_and_resting',
+                'light_activity',
+                'normal_activity',
+                'toilsome_activity',
+                'very_hard_activity',
+            ],
+            ActivityTypeCodes::getActivityTypeCodes()
+        );
+
+        $reflection = new \ReflectionClass('\DrdPlus\Codes\ActivityTypeCodes');
+        $constants = $reflection->getConstants();
+        foreach ($expectedActivityCodes as $expectedActivityCode) {
+            $constantName = strtoupper($expectedActivityCode);
+            self::assertArrayHasKey($constantName, $constants);
+            self::assertSame($constants[$constantName], $expectedActivityCode);
+        }
+    }
+}
