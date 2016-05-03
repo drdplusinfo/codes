c2c37073cb8552d982496021256c7c489f4f9398
jaroslavtyc <mail@jaroslavtyc.com>
2016-05-03 10:03:24 +0200

    Element codes

---
+++ DrdPlus/Tests/Codes/ElementCodesTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace DrdPlus\Tests\Codes;
+
+use DrdPlus\Codes\ElementCodes;
+
+class ElementCodesTest extends AbstractCodesTableTest
+{
+    /**
+     * @test
+     */
+    public function I_can_get_element_codes_at_once()
+    {
+        self::assertSame(
+            [
+                ElementCodes::FIRE,
+                ElementCodes::WATER,
+                ElementCodes::EARTH,
+                ElementCodes::AIR,
+            ],
+            ElementCodes::getElementCodes()
+        );
+    }
+}
