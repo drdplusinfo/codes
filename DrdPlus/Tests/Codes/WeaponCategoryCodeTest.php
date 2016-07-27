244961d81a2006de20502e209832f73ed68bde32
jaroslavtyc <mail@jaroslavtyc.com>
2016-07-27 15:54:04 +0200

    Weapon category code generalized to contains all weapon categories

---
+++ DrdPlus/Tests/Codes/WeaponCategoryCodeTest.php
@@ -0,0 +1,76 @@
+<?php
+namespace DrdPlus\Tests\Codes;
+
+use DrdPlus\Codes\WeaponCategoryCode;
+
+class WeaponCategoryCodeTest extends CodeTest
+{
+    /**
+     * @test
+     */
+    public function I_can_get_melee_weapon_category_codes_at_once()
+    {
+        self::assertSame(
+            $expectedCodes = [
+                'axe',
+                'knife_or_dagger',
+                'mace_or_club',
+                'morning_star_or_morgenstern',
+                'saber_or_bowie_knife',
+                'staff_or_spear',
+                'sword',
+                'voulge_or_trident',
+                'unarmed'
+            ],
+            WeaponCategoryCode::getMeleeWeaponCategoryCodes()
+        );
+        $this->I_can_get_codes_by_same_named_constants($expectedCodes);
+    }
+
+    /**
+     * test
+     */
+    public function I_can_get_range_weapon_category_codes()
+    {
+        self::assertSame(
+            $expectedCodes = [
+                'bow',
+                'crossbow',
+                'throwing_weapon',
+                'arrow',
+                'dart',
+                'sling_stone',
+            ],
+            WeaponCategoryCode::getRangeWeaponCategoryCodes()
+        );
+        $this->I_can_get_codes_by_same_named_constants($expectedCodes);
+    }
+
+    /**
+     * @test
+     */
+    public function I_can_get_all_codes_at_once()
+    {
+        self::assertSame(
+            [
+                'axe',
+                'knife_or_dagger',
+                'mace_or_club',
+                'morning_star_or_morgenstern',
+                'saber_or_bowie_knife',
+                'staff_or_spear',
+                'sword',
+                'voulge_or_trident',
+                'unarmed',
+                'bow',
+                'crossbow',
+                'throwing_weapon',
+                'arrow',
+                'dart',
+                'sling_stone',
+            ],
+            WeaponCategoryCode::getWeaponCategoryCodes()
+        );
+    }
+
+}
