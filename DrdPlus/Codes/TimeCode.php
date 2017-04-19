946e468e9fefce841bc390db329a08b119f03998
Jaroslav Týc <mail@jaroslavtyc.com>
2017-04-19 09:17:08 +0200

    Time code can give czech translations

--- DrdPlus/Codes/TimeCode.php
+++ DrdPlus/Codes/TimeCode.php
@@ -18,7 +18,7 @@ class TimeCode extends AbstractCode
     /**
      * @return array|string[]
      */
-    public static function getPossibleValues()
+    public static function getPossibleValues(): array
     {
         return [
             self::ROUND,
@@ -28,5 +28,53 @@ class TimeCode extends AbstractCode
             self::MONTH,
             self::YEAR,
         ];
+    }
+
+    /**
+     * @param string $languageCode
+     * @param string $plural
+     * @return string
+     */
+    public function translateTo(string $languageCode, string $plural = 'one'): string
+    {
+        $code = $this->getValue();
+        $translations = $this->getTranslations($languageCode);
+        if (($translations[$code][$plural] ?? null) !== null) {
+            return $translations[$code][$plural];
+        }
+        if ($languageCode === 'en') {
+            return str_replace('_', ' ', $code); // just replacing underscores by spaces
+        }
+        trigger_error(
+            "Missing translation for value '{$code}', language '{$languageCode}' and plural '{$plural}'"
+            . ', english will be used instead',
+            E_USER_WARNING
+        );
+        $translations = $this->getTranslations('en');
+        if (($translations[$code][$plural] ?? null) !== null) {
+            return $translations[$code][$plural]; // explicit english translation
+        }
+
+        return str_replace('_', ' ', $code); // just replacing underscores by spaces
+    }
+
+    private static $translations = [
+        'cs' => [
+            self::ROUND => ['one' => 'kolo', 'few' => 'kola', 'many' => 'kol'],
+            self::MINUTE => ['one' => 'minuta', 'few' => 'minuty', 'many' => 'minut'],
+            self::HOUR => ['one' => 'hodina', 'few' => 'hodiny', 'many' => 'hodin'],
+            self::DAY => ['one' => 'den', 'few' => 'dny', 'many' => 'dní'],
+            self::MONTH => ['one' => 'měsíc', 'few' => 'měsíce', 'many' => 'měsíců'],
+            self::YEAR => ['one' => 'rok', 'few' => 'roky', 'many' => 'let'],
+        ],
+    ];
+
+    /**
+     * @param string $languageCode
+     * @return array|string[]
+     */
+    protected function getTranslations(string $languageCode): array
+    {
+        return self::$translations[$languageCode] ?? [];
     }
 }
