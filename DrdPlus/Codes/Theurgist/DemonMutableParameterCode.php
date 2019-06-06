1a7631bb54184897a4331f5a3b1eaba285cc5a8e
Jaroslav Týc <mail@jaroslavtyc.com>
2019-06-06 18:43:05 +0200

    Demon mutable parameters renamed to clarify their purpose

--- DrdPlus/Codes/Theurgist/DemonMutableParameterCode.php
+++ DrdPlus/Codes/Theurgist/DemonMutableParameterCode.php
@@ -8,33 +8,33 @@ namespace DrdPlus\Codes\Theurgist;
  */
 class DemonMutableParameterCode extends AbstractTheurgistCode
 {
-    const CAPACITY = 'capacity';
-    const ENDURANCE = 'endurance';
-    const DURATION = 'duration';
-    const QUALITY = 'quality';
-    const RADIUS = 'radius';
-    const AREA = 'area';
-    const INVISIBILITY = 'invisibility';
-    const ARMOR = 'armor';
-    const SPEED = 'speed';
-    const STRENGTH = 'strength';
-    const AGILITY = 'agility';
-    const KNACK = 'knack';
+    const DEMON_CAPACITY = 'demon_capacity';
+    const DEMON_ENDURANCE = 'demon_endurance';
+    const DEMON_DURATION = 'demon_duration';
+    const DEMON_QUALITY = 'demon_quality';
+    const DEMON_RADIUS = 'demon_radius';
+    const DEMON_AREA = 'demon_area';
+    const DEMON_INVISIBILITY = 'demon_invisibility';
+    const DEMON_ARMOR = 'demon_armor';
+    const DEMON_SPEED = 'demon_speed';
+    const DEMON_STRENGTH = 'demon_strength';
+    const DEMON_AGILITY = 'demon_agility';
+    const DEMON_KNACK = 'demon_knack';
 
     private static $translations = [
         'cs' => [
-            self::CAPACITY => 'kapacita',
-            self::ENDURANCE => 'výdrž',
-            self::DURATION => 'doba trvání',
-            self::QUALITY => 'kvalita',
-            self::RADIUS => 'poloměr',
-            self::AREA => 'oblast',
-            self::INVISIBILITY => 'neviditelnost',
-            self::ARMOR => 'zbroj ',
-            self::SPEED => 'rychlost',
-            self::STRENGTH => 'síla',
-            self::AGILITY => 'obratnost',
-            self::KNACK => 'zručnost',
+            self::DEMON_CAPACITY => 'demon_kapacita',
+            self::DEMON_ENDURANCE => 'demon_výdrž',
+            self::DEMON_DURATION => 'demon_doba trvání',
+            self::DEMON_QUALITY => 'demon_kvalita',
+            self::DEMON_RADIUS => 'demon_poloměr',
+            self::DEMON_AREA => 'demon_oblast',
+            self::DEMON_INVISIBILITY => 'demon_neviditelnost',
+            self::DEMON_ARMOR => 'demon_zbroj ',
+            self::DEMON_SPEED => 'demon_rychlost',
+            self::DEMON_STRENGTH => 'demon_síla',
+            self::DEMON_AGILITY => 'demon_obratnost',
+            self::DEMON_KNACK => 'demon_zručnost',
         ],
     ];
 
