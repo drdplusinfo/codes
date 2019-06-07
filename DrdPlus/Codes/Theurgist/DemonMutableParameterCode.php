7fd595000d1890e9c935ace8c3dbb67db702c248
Jaroslav Týc <mail@jaroslavtyc.com>
2019-06-07 15:02:57 +0200

    Some demon mutable parameter codes renamed

--- DrdPlus/Codes/Theurgist/DemonMutableParameterCode.php
+++ DrdPlus/Codes/Theurgist/DemonMutableParameterCode.php
@@ -10,37 +10,37 @@ class DemonMutableParameterCode extends AbstractTheurgistCode
 {
     const DEMON_CAPACITY = 'demon_capacity';
     const DEMON_ENDURANCE = 'demon_endurance';
-    const DEMON_DURATION = 'demon_duration';
+    const DEMON_ACTIVATION_DURATION = 'demon_activation_duration';
     const DEMON_QUALITY = 'demon_quality';
     const DEMON_RADIUS = 'demon_radius';
     const DEMON_AREA = 'demon_area';
     const DEMON_INVISIBILITY = 'demon_invisibility';
     const DEMON_ARMOR = 'demon_armor';
-    const DEMON_SPEED = 'demon_speed';
+    const SPELL_SPEED = 'spell_speed';
     const DEMON_STRENGTH = 'demon_strength';
     const DEMON_AGILITY = 'demon_agility';
     const DEMON_KNACK = 'demon_knack';
 
-    private static $translations = [
-        'cs' => [
-            self::DEMON_CAPACITY => 'demon_kapacita',
-            self::DEMON_ENDURANCE => 'demon_výdrž',
-            self::DEMON_DURATION => 'demon_doba trvání',
-            self::DEMON_QUALITY => 'demon_kvalita',
-            self::DEMON_RADIUS => 'demon_poloměr',
-            self::DEMON_AREA => 'demon_oblast',
-            self::DEMON_INVISIBILITY => 'demon_neviditelnost',
-            self::DEMON_ARMOR => 'demon_zbroj ',
-            self::DEMON_SPEED => 'demon_rychlost',
-            self::DEMON_STRENGTH => 'demon_síla',
-            self::DEMON_AGILITY => 'demon_obratnost',
-            self::DEMON_KNACK => 'demon_zručnost',
-        ],
-    ];
-
-    protected function getTranslations(string $languageCode): array
+    protected function fetchTranslations(): array
     {
-        return static::$translations[$languageCode] ?? [];
+        return [
+            'cs' => [
+                'one' => [
+                    self::DEMON_CAPACITY => 'kapacita',
+                    self::DEMON_ENDURANCE => 'výdrž',
+                    self::DEMON_ACTIVATION_DURATION => 'doba trvání',
+                    self::DEMON_QUALITY => 'kvalita',
+                    self::DEMON_RADIUS => 'poloměr',
+                    self::DEMON_AREA => 'oblast',
+                    self::DEMON_INVISIBILITY => 'neviditelnost',
+                    self::DEMON_ARMOR => 'zbroj ',
+                    self::SPELL_SPEED => 'rychlost kouzla',
+                    self::DEMON_STRENGTH => 'síla',
+                    self::DEMON_AGILITY => 'obratnost',
+                    self::DEMON_KNACK => 'zručnost',
+                ],
+            ],
+        ];
     }
 
 }
